f7dd2914af4d7b3964f8b9203a75ca26e32c4fbd
deepak-mnt <deepak@mntechnique.com>
2017-11-28 18:39:47 +0530

    Update DocTypes of Accounts of erpnext

---
+++ rollon_erpnext/hooks_property_setter.py
@@ -0,0 +1,13 @@
+property_setter = {
+	"dt": "Property Setter",
+	"filters": [["name", "in", ["Journal Entry-user_remark-print_hide",
+								"Journal Entry-total_debit-in_list_view",
+								"Journal Entry-total_debit-print_hide",
+								"Journal Entry-total_credit-print_hide",
+								"Journal Entry-total_amount-in_list_view",
+								"Journal Entry-total_amount-print_hide",
+								"Journal Entry-total_amount-bold",
+								"Journal Entry-remark-in_global_search",
+								"Journal Entry-pay_to_recd_from-allow_on_submit",
+								"Journal Entry-read_only_onload"]]]
+}
