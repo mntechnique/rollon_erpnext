fd65f503e303dbd83bec79f210c88afbdb4defa9
Harsh <harshpatel0214@gmail.com>
2017-12-14 19:37:08 +0530

    Added changes in hooks_custom_field and hooks_property

--- rollon_erpnext/hooks_custom_field.py
+++ rollon_erpnext/hooks_custom_field.py
@@ -1,8 +1,12 @@
 custom_field = {
-	"dt":"Property Setter", 
-	"filters": [["name", "in", "Purchase Invoice-section_break_40"
-	"Purchase Invoice-form_types"
-	"Sales Invoice Item-customer_po_date"
+	"dt":"Custom Field", 
+	"filters": [["name", "in", "Purchase Invoice-section_break_40",
+	"Purchase Invoice-form_types",
+	"Sales Invoice Item-customer_po_date",
+	"Sales Invoice-customer_purchase_order",
+	"Sales Invoice-customer_purchase_order_date",
+	"Sales Invoice-mode_of_dispatch",
+	"Sales Invoice-total_qty"
 
 
 
--- rollon_erpnext/hooks_property_setter.py
+++ rollon_erpnext/hooks_property_setter.py
@@ -1,25 +1,30 @@
 property_setter = {
 	"dt":"Property Setter", 
-	"filters": [["name", "in", ["Journal Entry Account-account_currency-print_hide",
-	"Journal Entry Account-exchange_rate-print_hide",
-	"Journal Entry Account-sort_order",
-	"Journal Entry Account-read_only_onload"
-	"Purchase Order Item-price_list_rate-fieldtype"
-	"Purchase Order Item-rate-fieldtype"
-	"Purchase Order Item-amount-fieldtype"
-	"BOM-rm_cost_as_per-reqd"
-	"BOM-rm_cost_as_per-options"
-	"BOM-operations-read_only"
-	"BOM-operations_section-read_only"
-	"BOM Item-scrap-read_only"
-	"BOM Operation-sort_order"
-	"Customer-tax_id-read_only"
-	"Sales Order-taxes-hidden"
-	"Company-abbr-in_list_view"
-	"Currency Exchange-sort_order"
-	"ToDo-sort_order"
-	"Purchase Taxes and Charges Template-sort_order"
-	"Sales Invoice-default_print_format"
-	"Sales Invoice Item-customer_item_code-label"
-	"Sales Invoice Item-item_code-label"]]]
+	"filters": [
+		[
+			"name", "in", [
+				"Journal Entry Account-account_currency-print_hide",
+				"Journal Entry Account-sort_order",
+				"Purchase Order Item-price_list_rate-fieldtype"
+				"Purchase Order Item-rate-fieldtype"
+				"Purchase Order Item-amount-fieldtype"
+				"BOM-rm_cost_as_per-reqd"
+				"BOM-rm_cost_as_per-options"
+				"BOM-operations-read_only"
+				"BOM-operations_section-read_only"
+				"BOM Item-scrap-read_only"
+				"BOM Operation-sort_order"
+				"Sales Order-taxes-hidden"
+				"Company-abbr-in_list_view"
+				"Currency Exchange-sort_order"
+				"ToDo-sort_order"
+				"Purchase Taxes and Charges Template-sort_order"
+				"Sales Invoice-default_print_format"
+				"Sales Invoice Item-customer_item_code-label"
+				"Sales Invoice Item-item_code-label"
+				"Sales Taxes and Charges-sort_order"
+				"Sales Invoice-taxes_and_charges-reqd"
+				]
+			]
+	]
 }
