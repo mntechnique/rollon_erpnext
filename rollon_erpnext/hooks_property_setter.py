0b690fa741149811f436f5e1abc2fedfe4a1680d
Harsh <harshpatel0214@gmail.com>
2017-12-19 13:20:55 +0530

    Added changes in property setter

--- rollon_erpnext/hooks_property_setter.py
+++ rollon_erpnext/hooks_property_setter.py
@@ -5,25 +5,25 @@ property_setter = {
 			"name", "in", [
 				"Journal Entry Account-account_currency-print_hide",
 				"Journal Entry Account-sort_order",
-				"Purchase Order Item-price_list_rate-fieldtype"
-				"Purchase Order Item-rate-fieldtype"
-				"Purchase Order Item-amount-fieldtype"
-				"BOM-rm_cost_as_per-reqd"
-				"BOM-rm_cost_as_per-options"
-				"BOM-operations-read_only"
-				"BOM-operations_section-read_only"
-				"BOM Item-scrap-read_only"
-				"BOM Operation-sort_order"
-				"Sales Order-taxes-hidden"
-				"Company-abbr-in_list_view"
-				"Currency Exchange-sort_order"
-				"ToDo-sort_order"
-				"Purchase Taxes and Charges Template-sort_order"
-				"Sales Invoice-default_print_format"
-				"Sales Invoice Item-customer_item_code-label"
-				"Sales Invoice Item-item_code-label"
-				"Sales Taxes and Charges-sort_order"
-				"Sales Invoice-taxes_and_charges-reqd"
+				"Purchase Order Item-price_list_rate-fieldtype",
+				"Purchase Order Item-rate-fieldtype",
+				"Purchase Order Item-amount-fieldtype",
+				"BOM-rm_cost_as_per-reqd",
+				"BOM-rm_cost_as_per-options",
+				"BOM-operations-read_only",
+				"BOM-operations_section-read_only",
+				"BOM Item-scrap-read_only",
+				"BOM Operation-sort_order",
+				"Sales Order-taxes-hidden",
+				"Company-abbr-in_list_view",
+				"Currency Exchange-sort_order",
+				"ToDo-sort_order",
+				"Purchase Taxes and Charges Template-sort_order",
+				"Sales Invoice-default_print_format",
+				"Sales Invoice Item-customer_item_code-label",
+				"Sales Invoice Item-item_code-label",
+				"Sales Taxes and Charges-sort_order",
+				"Sales Invoice-taxes_and_charges-reqd",
 				]
 			]
 	]
